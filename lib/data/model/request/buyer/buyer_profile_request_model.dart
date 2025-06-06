96c9e2c133668a334eb3b135d6a52650ee2015a4
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-06-06 18:04:52 +0700

    menambahkan konstruktor dan metode copywith

--- lib/data/model/request/buyer/buyer_profile_request_model.dart
+++ lib/data/model/request/buyer/buyer_profile_request_model.dart
@@ -5,3 +5,20 @@ class BuyerProfileRequestModel {
   final String? address;
   final String? phone;
   final String? photo;
+
+
+ BuyerProfileRequestModel({this.name, this.address, this.phone, this.photo});
+
+  BuyerProfileRequestModel copyWith({
+    String? name,
+    String? address,
+    String? phone,
+    String? photo,
+  }) {
+    return BuyerProfileRequestModel(
+      name: name ?? this.name,
+      address: address ?? this.address,
+      phone: phone ?? this.phone,
+      photo: photo ?? this.photo,
+    );
+  }
