aa485661474c3578f1e0b87294442d96164df25f
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-06-06 18:02:57 +0700

    menambahkan metode untuk mengubah objek JSON

--- lib/data/model/request/auth/register_request_model.dart
+++ lib/data/model/request/auth/register_request_model.dart
@@ -12,3 +12,22 @@ class RegisterRequestModel {
         this.password,
         this.roleId = 2,
     });
+
+    factory RegisterRequestModel.fromJson(String str) => RegisterRequestModel.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory RegisterRequestModel.fromMap(Map<String, dynamic> json) => RegisterRequestModel(
+        username: json["username"],
+        email: json["email"],
+        password: json["password"],
+        roleId: json["role_id"],
+    );
+
+    Map<String, dynamic> toMap() => {
+        "username": username,
+        "email": email,
+        "password": password,
+        "role_id": roleId,
+    };
+}
