24137c4985c2a9236dbd36a7b868455f506c3827
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-06-06 17:36:22 +0700

    menambahkan inisiasi data anak burung

--- lib/data/model/request/admin/anak_request_model.dart
+++ lib/data/model/request/admin/anak_request_model.dart
@@ -9,3 +9,14 @@ class AnakRequestModel {
   final String? gambarBurung; // nullable
   final int ayahId;
   final int ibuId;
+
+    AnakRequestModel({
+    required this.noRing,
+    required this.tanggalLahir,
+    required this.jenisKelamin,
+    required this.jenisKenari,
+    required this.keterangan,
+    this.gambarBurung, // nullable constructor param
+    required this.ayahId,
+    required this.ibuId,
+  });
