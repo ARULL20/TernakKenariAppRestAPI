33e792302815ae2754c1eba2527bd004215d92b3
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-06-06 20:00:59 +0700

    menambahkan metode untuk mengonversi objek

--- lib/data/model/response/auth/login_response_model.dart
+++ lib/data/model/response/auth/login_response_model.dart
@@ -10,3 +10,20 @@ class LoginResponseModel {
         this.statusCode,
         this.data,
     });
+
+    factory LoginResponseModel.fromJson(String str) => LoginResponseModel.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory LoginResponseModel.fromMap(Map<String, dynamic> json) => LoginResponseModel(
+        message: json["message"],
+        statusCode: json["status_code"],
+        data: json["data"] == null ? null : Data.fromMap(json["data"]),
+    );
+
+    Map<String, dynamic> toMap() => {
+        "message": message,
+        "status_code": statusCode,
+        "data": data?.toMap(),
+    };
+}
