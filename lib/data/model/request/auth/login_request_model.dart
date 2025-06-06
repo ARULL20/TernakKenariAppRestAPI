c24f5a1e02f0b1b78545c021a825e998dfc4a0f8
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-06-06 17:59:53 +0700

    mengonversi objek dari format JSONn ke String atau map string

--- lib/data/model/request/auth/login_request_model.dart
+++ lib/data/model/request/auth/login_request_model.dart
@@ -8,3 +8,18 @@ class LoginRequestModel {
         this.email,
         this.password,
     });
+
+    factory LoginRequestModel.fromJson(String str) => LoginRequestModel.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory LoginRequestModel.fromMap(Map<String, dynamic> json) => LoginRequestModel(
+        email: json["email"],
+        password: json["password"],
+    );
+
+    Map<String, dynamic> toMap() => {
+        "email": email,
+        "password": password,
+    };
+}
