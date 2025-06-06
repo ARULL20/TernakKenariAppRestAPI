1060d75c74f93d324281851ec4da46962615f003
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-06-06 20:27:39 +0700

    implementasi repository yang mengelola operasi data burung anak anak kenari lewat HTTP client khusus ServiceHttpClient

--- lib/service/service_http_client.dart
+++ lib/service/service_http_client.dart
@@ -3,15 +3,13 @@ import 'dart:convert';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
-
 class ServiceHttpClient {
   final String baseUrl = 'http://10.0.0.2:8000/api/';
   final secureStorage = FlutterSecureStorage();
 
-
-  //POST
+  //post
   Future<http.Response> post(String endPoint, Map<String, dynamic> body) async {
-    final url = Uri.parse("baseUrl$endPoint");
+    final url = Uri.parse("$baseUrl$endPoint");
 
     try {
       final response = await http.post(
@@ -24,15 +22,54 @@ class ServiceHttpClient {
       );
       return response;
     } catch (e) {
-      throw Exception("Post request failed: $e");
+      throw Exception("POST request failed: $e");
     }
   }
-  //GET
+
+  //postwith token
+  Future<http.Response> postWihToken(
+    String endPoint,
+    Map<String, dynamic> body,
+  ) async {
+    final token = await secureStorage.read(key: "authToken");
+    final url = Uri.parse("$baseUrl$endPoint");
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(body),
+      );
+
+      return response;
+    } catch (e) {
+      throw Exception("POST request failed: $e");
+    }
+  }
+
+  //get
   Future<http.Response> get(String endPoint) async {
-    final token = await secureStorage.read(key: "token");
+    final token = await secureStorage.read(key: "authToken");
     final url = Uri.parse("$baseUrl$endPoint");
     try {
-      final response
+      final response = await http.get(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      );
+
+      return response;
+    } catch (e) {
+      throw Exception("GET request failed: $e");
     }
   }
+
+  //put
 }
