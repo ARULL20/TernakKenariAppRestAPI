fdc63ff85c4d4d8005dfff2b6de86cbced820c71
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-06-06 17:35:14 +0700

    menambahkan data dari anak request model

---
+++ lib/data/model/request/admin/anak_request_model.dart
@@ -0,0 +1,11 @@
+mport 'dart:convert';
+
+class AnakRequestModel {
+  final String noRing;
+  final DateTime tanggalLahir;
+  final String jenisKelamin;
+  final String jenisKenari;
+  final String keterangan;
+  final String? gambarBurung; // nullable
+  final int ayahId;
+  final int ibuId;
