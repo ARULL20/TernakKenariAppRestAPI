2c251c971f0778b01781173f3b51cf1558d0b278
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-06-06 17:50:03 +0700

    untuk konversi data antara JSON dan objek Dart

--- lib/data/model/request/admin/induk_request_model.dart
+++ lib/data/model/request/admin/induk_request_model.dart
@@ -32,3 +32,18 @@ class IndukRequestModel {
     keterangan: keterangan ?? this.keterangan,
     gambarBurung: gambarBurung ?? this.gambarBurung,
   );
+
+  factory IndukRequestModel.fromRawJson(String str) =>
+      IndukRequestModel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory IndukRequestModel.fromJson(Map<String, dynamic> json) =>
+      IndukRequestModel(
+        noRing: json["no_ring"],
+        tanggalLahir: DateTime.parse(json["tanggal_lahir"]),
+        jenisKelamin: json["jenis_kelamin"],
+        jenisKenari: json["jenis_kenari"],
+        keterangan: json["keterangan"],
+        gambarBurung: json["gambar_burung"], // nullable, can be null
+      );
