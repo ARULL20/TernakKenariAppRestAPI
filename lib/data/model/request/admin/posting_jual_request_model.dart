e96ec1222715ee2400ba5103cc31955fa29b7567
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-06-06 17:56:41 +0700

    untuk mengonversi data JSON menjadi objek

--- lib/data/model/request/admin/posting_jual_request_model.dart
+++ lib/data/model/request/admin/posting_jual_request_model.dart
@@ -12,3 +12,28 @@ class PostingJualRequestModel {
     required this.harga,
     required this.deskripsi,
   });
+
+  PostingJualRequestModel copyWith({
+    int? burungId,
+    String? burungType,
+    int? harga,
+    String? deskripsi,
+  }) => PostingJualRequestModel(
+    burungId: burungId ?? this.burungId,
+    burungType: burungType ?? this.burungType,
+    harga: harga ?? this.harga,
+    deskripsi: deskripsi ?? this.deskripsi,
+  );
+
+  factory PostingJualRequestModel.fromRawJson(String str) =>
+      PostingJualRequestModel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory PostingJualRequestModel.fromJson(Map<String, dynamic> json) =>
+      PostingJualRequestModel(
+        burungId: json["burung_id"],
+        burungType: json["burung_type"],
+        harga: json["harga"],
+        deskripsi: json["deskripsi"],
+      );
